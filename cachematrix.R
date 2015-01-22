5b58ab333153b56ce8bc1f684aabfe977582db3d
mulrooneyp <mulrooneyp@gmail.com>
2015-01-22 21:14:24 +0000

    Updated Commen

--- cachematrix.R
+++ cachematrix.R
@@ -2,12 +2,12 @@
 ## It consists of a pair of functions that cache the inverse of an invertible matrix.
 ##    1) makeCacheMatrix
 ##    2) cacheSolve
-##  pass your invertible matrix into makeCacheMatrix, which returns a specially formated list
-##  pass this special list into cacheSolve & it will retun the inverse of the original matrix
-##  if the inverse has already been calculated, it uses a "cached" calcuation
+##  pass your invertible matrix into makeCacheMatrix, which returns a specially formatted list
+##  pass this special list into cacheSolve & it will return the inverse of the original matrix
+##  if the inverse has already been calculated, it uses a cached calcuation
 
 
-##  makeCacheMatrix creates a special "Matrix", which is really a list containing a function to
+##  makeCacheMatrix creates a special "matrix", which is really a list containing a function to
 ##    1) set the value of the Matix
 ##    2) get the value of the Matix
 ##    3) set the value of the Inverse Matix
@@ -33,7 +33,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-##  cacheSolve,  calculates the Inverse of the original (contained in the list returnd from the makeCacheMatrix function
+##  cacheSolve,  calculates the Inverse of the original matrix 
+##    contained in the list returned from the makeCacheMatrix function
 ##    However, it first checks to see if the Inverse has already been calculated.
 ##    If so, it gets the Inverse from the cache and skips the computation. 
 ##    Otherwise, it calculates the Inverse of the data and sets the value of the Inverse in the cache 
@@ -41,7 +42,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ##    It return a matrix that is the inverse of original matrix
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
   
   m <- x$getInverse()
   if(!is.null(m)) {
